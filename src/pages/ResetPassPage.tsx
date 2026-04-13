eb387fe0a1889663de98a16850ab49f873b4b9a3
ItsTerryTheBerry <165344016+Terry-The-Berry@users.noreply.github.com>
2026-04-13 00:55:53 -0400

    Rename component to match file name

--- src/pages/ResetPassPage.tsx
+++ src/pages/ResetPassPage.tsx
@@ -1,14 +1,13 @@
 import PageTitle from '../components/PageTitle';
 //import LoggedInName from '../components/LoggedInName';
 
-const resetPassPage = () =>
+const ResetPassPage = () =>
 {
     return(
         <div>
             <PageTitle />
-            <p>Email verified! you can now close this tab</p>
         </div>
     );
 }
 
-export default resetPassPage;
+export default ResetPassPage;
