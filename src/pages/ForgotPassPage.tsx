74ff5d915915687aef0c7f88503b296f1bde1da9
ItsTerryTheBerry <165344016+Terry-The-Berry@users.noreply.github.com>
2026-04-13 01:46:22 -0400

    Created ForgotPassPage.tsx

---
+++ src/pages/ForgotPassPage.tsx
@@ -0,0 +1,14 @@
+import PageTitle from '../components/PageTitle';
+//import LoggedInName from '../components/LoggedInName';
+
+const ForgotPassPage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <p>FORGOT PASSWORD PAGE</p>
+        </div>
+    );
+}
+
+export default ForgotPassPage;
