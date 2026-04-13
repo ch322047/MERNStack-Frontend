0f455eeae0d65366f0752514636a1ead378eaf4c
ItsTerryTheBerry <165344016+Terry-The-Berry@users.noreply.github.com>
2026-04-13 01:00:20 -0400

    Create VerifyEmailPage.tsx

---
+++ src/pages/VerifyEmailPage.tsx
@@ -0,0 +1,14 @@
+import PageTitle from '../components/PageTitle';
+//import LoggedInName from '../components/LoggedInName';
+
+const VerifyEmailPage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <p>Your email has been verified! You can now close this tab.</p>
+        </div>
+    );
+}
+
+export default VerifyEmailPage;
