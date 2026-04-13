85e1deb5fdfa71318d6e531f437d8322f7a04a70
ItsTerryTheBerry <165344016+Terry-The-Berry@users.noreply.github.com>
2026-04-13 10:57:09 -0400

    Update ResetPassword.tsx

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -5,33 +5,24 @@ import '../index.css';
 function ResetPassword() {
   const navigate = useNavigate();
 
-  const [isLoginTab, setIsLoginTab] = useState(true);
   const [message, setMessage] = useState('');
 
   // Login
-  const [loginName, setLoginName] = useState('');
   const [loginPassword, setLoginPassword] = useState('');
 
-  const [loginCode, setLoginCode] = useState('');
-  const [showCodeInput, setShowCodeInput] = useState(false);
-
   // Register
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [registerLogin, setRegisterLogin] = useState('');
-  const [email, setEmail] = useState('');
-  const [registerPassword, setRegisterPassword] = useState('');
+  //const [newPassword, setNewPassword] = useState('');
 
   function buildPath(route: string) {
     return `https://lampstackprojectgroup9.com/api/${route}`;
   }
 
-  async function doLogin(e: React.FormEvent) {
+  async function reset(e: React.FormEvent) {
     e.preventDefault();
     setMessage('');
 
     try {
-      const response = await fetch(buildPath('login'), {
+      const response = await fetch(buildPath('reset-password'), {
         method: 'POST',
         body: JSON.stringify({ login: loginName, password: loginPassword }),
         headers: { 'Content-Type': 'application/json' },
@@ -51,32 +42,7 @@ function ResetPassword() {
     }
   }
 
-  async function verifyLoginCode(e: React.FormEvent) {
-    e.preventDefault();
-    setMessage('');
-
-    try {
-      const response = await fetch(buildPath('verify-login-code'), {
-        method: 'POST',
-        body: JSON.stringify({ login: loginName, code: loginCode }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-
-      const res = await response.json();
-
-      if (res.error) {
-        setMessage(res.error);
-      } else {
-        // Store user info & token
-        localStorage.setItem('user_data', JSON.stringify(res.user));
-        localStorage.setItem('token', res.token);
-        navigate('/trips');
-      }
-    } catch (err: any) {
-      setMessage(err.toString());
-    }
-  }
-
+  /*
   async function doRegister(e: React.FormEvent) {
     e.preventDefault();
     setMessage('');
@@ -116,44 +82,8 @@ function ResetPassword() {
     } catch (err: any) {
       setMessage(err.toString());
     }
-  }
+  }*/
 
-  // sends the username and email to the backend, triggering a resent email
-  async function resendEmail(e: React.FormEvent) {
-    e.preventDefault();
-    setMessage('');
-
-    try {
-      const response = await fetch(buildPath('resend-verification-email'), {
-        method: 'POST',
-        body: JSON.stringify({
-          login: registerLogin,
-          email,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-
-      let res;
-      try {
-        res = await response.json()
-      } catch (err) {
-        console.error('Failed to parse JSON.');
-        setMessage('Server error: see console');
-        return;
-      }
-
-      if (res.error) {
-        setMessage(res.error);
-      } else {
-        //localStorage.setItem('user_data', JSON.stringify(res.user));
-        setMessage('Email Resent! Check your email.');
-      }
-        
-      
-    } catch (err: any) {
-      setMessage(err.toString());
-    }
-  }
 
   
   return (
@@ -171,37 +101,24 @@ function ResetPassword() {
         <h1>Reset Password</h1>
 
         
-        <>
-          {!showCodeInput ? (
-            <form onSubmit={doLogin}>
-              <input
-                type="text"
-                placeholder="Username"
-                value={loginName}
-                onChange={(e) => setLoginName(e.target.value)}
-              />
+        
+        <form onSubmit={reset}>
+          <input
+            type="password"
+            placeholder="Old Password"
+            value={loginPassword}
+            onChange={(e) => setLoginPassword(e.target.value)}
+          />
 
-              <input
-                type="password"
-                placeholder="Password"
-                value={loginPassword}
-                onChange={(e) => setLoginPassword(e.target.value)}
-              />
+          <input
+            type="password"
+            placeholder="New Password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
 
-              <button type="submit">Login</button>
-            </form>
-          ) : (
-            <form onSubmit={verifyLoginCode}>
-              <input
-                type="text"
-                placeholder="Enter code from email"
-                value={loginCode}
-                onChange={(e) => setLoginCode(e.target.value)}
-              />
-              <button type="submit">Verify Code</button>
-            </form>
-          )}
-        </>
+          <button type="submit">Reset</button>
+        </form>
         
 
         <p className="message">{message}</p>
