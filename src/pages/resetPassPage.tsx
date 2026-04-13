68a7326b92f267e4f9fa1949a9a38370220ceb72
ItsTerryTheBerry <165344016+Terry-The-Berry@users.noreply.github.com>
2026-04-13 00:49:15 -0400

    Created resetPassPage.tsx

---
+++ src/pages/resetPassPage.tsx
@@ -0,0 +1,14 @@
+import PageTitle from '../components/PageTitle';
+//import LoggedInName from '../components/LoggedInName';
+
+const resetPassPage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <p>Email verified! you can now close this tab</p>
+        </div>
+    );
+}
+
+export default resetPassPage;
