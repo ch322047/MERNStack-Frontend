a36ecc8ca0a4b093159941cc6663409f9a5b4d8e
ItsTerryTheBerry <165344016+Terry-The-Berry@users.noreply.github.com>
2026-04-13 21:14:49 -0400

    Clean up ResetPassPage component
    Removed unused imports and localStorage logic.

--- src/pages/ResetPassPage.tsx
+++ src/pages/ResetPassPage.tsx
@@ -1,18 +1,8 @@
-import { useEffect } from 'react';
 import PageTitle from '../components/PageTitle';
 import ResetPassword from '../components/ResetPassword';
-import { useNavigate } from 'react-router-dom';
-//import LoggedInName from '../components/LoggedInName';
 
 const ResetPassPage = () =>
 {
-    const navigate = useNavigate();
-    
-    // Get user from localStorage
-    const stored = localStorage.getItem('user_data');
-    const user = stored ? JSON.parse(stored) : null;
-    const userId = user?.id;
-    const token = localStorage.getItem('token');
 
     
     return(
